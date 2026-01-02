759e64fe322decb170b9694bf0ca04325bc4d058
Zayveeuhr <daelynxwinston@gmail.com>
2026-01-02 06:43:09 -0500

    Working on exercises to test my understanding.

---
+++ Ch03Ex03Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿
+#region Increment and addition operators
+
+//int x = 3;
+
+//int y = 2 + ++x;
+
+//WriteLine($"X is {x} and y is {y}");
+
+#endregion
+
+#region Binary shift operators
+
+//int x = 3 << 2;
+
+//int y = 10 >> 1;
+
+//WriteLine($"X is {x} and Y is {y}");
+
+#endregion
+
+#region Bitwise operators
+
+int x = 10 & 8;
+
+int y = 10 | 7;
+
+WriteLine($"X is {x} and Y is {y}");
+
+#endregion
---
+++ Exercise_Exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿
+WriteLine("Enter a number between 0 and 255: ");
+string? firstNum = ReadLine();
+
+WriteLine("Enter another number between 0 and 255: ");
+string? secondNum = ReadLine();
+
+try
+{
+    byte inputOne = byte.Parse(firstNum!);
+    byte inputTwo = byte.Parse(secondNum!);
+
+    WriteLine($"{inputOne} divided by {inputTwo} is {inputOne / inputTwo}");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType().Name}: {ex.Message}");
+}
